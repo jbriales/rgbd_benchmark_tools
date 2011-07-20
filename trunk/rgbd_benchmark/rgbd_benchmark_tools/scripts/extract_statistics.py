ce648ab2984ab368a4421664e0cea18d3a253021
sturmju <sturmju@1019801f-97d9-4986-93c4-4f567f763985>
2011-07-20 17:18:15 +0000

    update

--- trunk/rgbd_benchmark/rgbd_benchmark_tools/scripts/extract_statistics.py
+++ trunk/rgbd_benchmark/rgbd_benchmark_tools/scripts/extract_statistics.py
@@ -37,10 +37,11 @@ if __name__ == '__main__':
     for axis in range(3):
         values = [traj_gt[t][axis,3] for t in keys]
         #print values
-        print axis, numpy.max(values)-numpy.min(values)
+        print "%s-axis: %0.2fm"%(["x","y","z"][axis],numpy.max(values)-numpy.min(values))
         
     delta = 1
     motion_trans = []
+    motion_rot = []
     for t_0 in keys:
         if t_0+delta > keys[-1]: 
             break
@@ -51,7 +52,22 @@ if __name__ == '__main__':
         quat = tuple(tf.transformations.quaternion_from_matrix(motion44))
         
         motion_trans.append( numpy.linalg.norm(xyz) )
+        
+        # an invitation to 3-d vision, p 27
+        motion_rot.append( numpy.arccos( (numpy.trace(motion44[0:3,0:3]) - 1)/2) )
+        
+    avg_delta = numpy.mean([a-b for a,b in zip(keys[1:],keys[:-1])])
+        
     print
-    print "average speed (m/s): ",numpy.mean(motion_trans)
+    print "average groundtruth frequency: %0.3fHz"%(1/avg_delta)
+    print "average translational velocity: %0.3fm/s"%numpy.mean(motion_trans)
+    print "average angular velocity: %0.3frad/s (%0.3fdeg/s)"%(
+                                                         numpy.mean(motion_rot),
+                                                         numpy.mean(motion_rot) / numpy.pi * 180)
+    print
+    print "trajectory length, translational motion: %0.3fm"%(numpy.sum(motion_trans)*avg_delta)
+    print "trajectory length, rotational motion: %0.3frad (%0.3fdeg)"%(
+                                                         numpy.sum(motion_rot)*avg_delta,
+                                                         numpy.sum(motion_rot) / numpy.pi * 180*avg_delta)
         
     
