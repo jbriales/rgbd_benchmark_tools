20540b7504a0a59327b42e00b2092b44b7d1687e
sturmju <sturmju@1019801f-97d9-4986-93c4-4f567f763985>
2011-07-21 10:24:21 +0000

    update

--- scripts/extract_statistics.py
+++ scripts/extract_statistics.py
@@ -21,6 +21,7 @@ from evaluate import *
 
 
 if __name__ == '__main__':
+    output = []
     
     # parse command line
     parser = argparse.ArgumentParser(description='''
@@ -36,7 +37,7 @@ if __name__ == '__main__':
     for axis in range(3):
         values = [traj_gt[t][axis,3] for t in keys]
         #print values
-        print "motion along %s-axis: %0.2fm"%(["x","y","z"][axis],numpy.max(values)-numpy.min(values))
+        output.append("dimensions.%s=%0.2fm"%(["x","y","z"][axis],numpy.max(values)-numpy.min(values)))
         
     delta = 1
     motion_trans = []
@@ -55,20 +56,50 @@ if __name__ == '__main__':
         # an invitation to 3-d vision, p 27
         motion_rot.append( numpy.arccos( (numpy.trace(motion44[0:3,0:3]) - 1)/2) )
         
-    avg_delta = numpy.mean([a-b for a,b in zip(keys[1:],keys[:-1])])
-        
     
-    print
-    print "average groundtruth frequency: %0.3fHz"%(1/avg_delta)
-    print "duration: %0.1fs"%(keys[-1]-keys[0])
-    print "average translational velocity: %0.3fm/s"%numpy.mean(motion_trans)
-    print "average angular velocity: %0.3frad/s (%0.3fdeg/s)"%(
-                                                         numpy.mean(motion_rot),
-                                                         numpy.mean(motion_rot) / numpy.pi * 180)
-    print
-    print "sum of translations along trajectory: %0.3fm"%(numpy.sum(motion_trans)*avg_delta)
-    print "angular sum of rotations motion along trajectory: %0.3frad (%0.3fdeg)"%(
-                                                         numpy.sum(motion_rot)*avg_delta,
-                                                         numpy.sum(motion_rot) / numpy.pi * 180*avg_delta)
-        
+    
+    median_groundtruth_freq = 1/numpy.median([a-b for a,b in zip(keys[1:],keys[:-1])])
+    output.append("groundtruth_freq.median=%0.2fHz"%median_groundtruth_freq)
+    
+    avg_groundtruth_freq = 1/numpy.mean([a-b for a,b in zip(keys[1:],keys[:-1])])
+    output.append("groundtruth_freq.mean=%0.2fHz"%avg_groundtruth_freq)
+    
+    duration = keys[-1]-keys[0]
+    output.append("duration=%0.2fs"%duration)
+
+    duration_groundtruth = 1/median_groundtruth_freq * len(keys)
+    output.append("duration.with_groundtruth=%0.2fs"%duration_groundtruth)
+
+    output.append("translational_velocity.mean=%0.3fm/s"%numpy.mean(motion_trans))
+    output.append("translational_velocity.median=%0.3fm/s"%numpy.median(motion_trans))
+    output.append("translational_velocity.min=%0.3fm/s"%numpy.min(motion_trans))
+    output.append("translational_velocity.max=%0.3fm/s"%numpy.max(motion_trans))
+    #output.append("translational_velocity.p5=%0.3fm/s"%numpy.percentile(motion_trans,5))
+    #output.append("translational_velocity.p95=%0.3fm/s"%numpy.percentile(motion_trans,95))
+
+    output.append("angular_velocity.mean=%0.3frad/s"%(numpy.mean(motion_rot)))
+    output.append("angular_velocity.median=%0.3frad/s"%(numpy.median(motion_rot)))
+    output.append("angular_velocity.min=%0.3frad/s"%(numpy.min(motion_rot)))
+    output.append("angular_velocity.max=%0.3frad/s"%(numpy.max(motion_rot)))
+    #output.append("angular_velocity.p5=%0.3frad/s"%(numpy.percentile(motion_rot,5)))
+    #output.append("angular_velocity.p95=%0.3frad/s"%(numpy.percentile(motion_rot,95)))
+
+    output.append("angular_velocity.mean.deg=%0.3fdeg/s"%(numpy.mean(motion_rot)/numpy.pi*180))
+    output.append("angular_velocity.median.deg=%0.3fdeg/s"%(numpy.median(motion_rot)/numpy.pi*180))
+    output.append("angular_velocity.min.deg=%0.3fdeg/s"%(numpy.min(motion_rot)/numpy.pi*180))
+    output.append("angular_velocity.max.deg=%0.3fdeg/s"%(numpy.max(motion_rot)/numpy.pi*180))
+    #output.append("angular_velocity.p5.deg=%0.3fdeg/s"%(numpy.percentile(motion_rot,5)/numpy.pi*180))
+    #output.append("angular_velocity.p95.deg=%0.3fdeg/s"%(numpy.percentile(motion_rot,95)/numpy.pi*180))
+
+    output.append("trajectory_length.translational=%0.3fm"%(numpy.sum(motion_trans)/avg_groundtruth_freq))    
+    output.append("trajectory_length.rotational=%0.3frad/s"%(numpy.sum(motion_rot)/avg_groundtruth_freq))
+    output.append("trajectory_length.rotational.deg=%0.3fdeg/s"%(numpy.sum(motion_rot) / numpy.pi * 180/avg_groundtruth_freq))
+                  
+    output = "\n".join(output)
+    
+    f = open( os.path.splitext(args.groundtruth)[0] + "-statistics.txt", "w")
+    f.write(output+"\n")
+    f.close
+    
+    print output
     
