44614b5405839a276640c9aac8eeda0e1ef1a6fa
sturmju <sturmju@1019801f-97d9-4986-93c4-4f567f763985>
2011-07-25 14:53:05 +0000

    update

--- scripts/evaluate.py
+++ scripts/evaluate.py
@@ -40,6 +40,7 @@ def ominus(a,b):
 
 def compute_err(param_delta,param_delay,traj_gt,traj_est,stamps_gt,stamps_est):
     err_trans = []
+    err_rot = []
     for stamp_est_0 in stamps_est:
         if stamp_est_0+param_delta > stamps_est[-1]: 
             break
@@ -55,7 +56,29 @@ def compute_err(param_delta,param_delay,traj_gt,traj_est,stamps_gt,stamps_est):
         quat = tuple(tf.transformations.quaternion_from_matrix(error44))
         
         err_trans.append( numpy.linalg.norm(xyz) )
-    return [numpy.mean(err_trans),numpy.std(err_trans)]
+        # an invitation to 3-d vision, p 27
+        err_rot.append( numpy.arccos( (numpy.trace(error44[0:3,0:3]) - 1)/2) )
+        
+    output =[]
+    output.append("translational_error.mean=%0.3fm"%(numpy.mean(err_trans)))
+    output.append("translational_error.std=%0.3fm"%(numpy.std(err_trans)))
+    output.append("translational_error.median=%0.3fm"%(numpy.median(err_trans)))
+    output.append("translational_error.min=%0.3fm"%(numpy.min(err_trans)))
+    output.append("translational_error.max=%0.3fm"%(numpy.max(err_trans)))
+    
+    output.append("rotational_error.mean=%0.3frad"%(numpy.mean(err_rot)))
+    output.append("rotational_error.std=%0.3frad"%(numpy.std(err_rot)))
+    output.append("rotational_error.median=%0.3frad"%(numpy.median(err_rot)))
+    output.append("rotational_error.min=%0.3frad"%(numpy.min(err_rot)))
+    output.append("rotational_error.max=%0.3frad"%(numpy.max(err_rot)))
+    
+    output.append("rotational_error.mean.deg=%0.3fdeg"%(numpy.mean(err_rot)/numpy.pi*180))
+    output.append("rotational_error.std.deg=%0.3fdeg"%(numpy.std(err_rot)/numpy.pi*180))
+    output.append("rotational_error.median.deg=%0.3fdeg"%(numpy.median(err_rot)/numpy.pi*180))
+    output.append("rotational_error.min.deg=%0.3fdeg"%(numpy.min(err_rot)/numpy.pi*180))
+    output.append("rotational_error.max.deg=%0.3fdeg"%(numpy.max(err_rot)/numpy.pi*180))
+    
+    return output
 
 
 if __name__ == '__main__':
@@ -68,8 +91,8 @@ if __name__ == '__main__':
     parser.add_argument('estimated', help='estimated trajectory file (format: timestamp x y z qx qy qz qw)')
     parser.add_argument('--delta', help='time delta for evaluation (see paper)',default=1.0)
     parser.add_argument('--delay', help='time offset between files (0.0 if synced, otherwise )',default=0.0)
-    parser.add_argument('--evaldelta', help='evaluate over different time deltas')
-    parser.add_argument('--evaldelay', help='evaluate over different time delays')
+#    parser.add_argument('--evaldelta', help='evaluate over different time deltas')
+#    parser.add_argument('--evaldelay', help='evaluate over different time delays')
     args = parser.parse_args()
     
     traj_gt = read_trajectory(args.groundtruth)
@@ -82,17 +105,22 @@ if __name__ == '__main__':
     stamps_est = list(traj_est.keys())
     stamps_est.sort()
 
-    if args.evaldelta:
-        for param_delta in numpy.arange(0,10,0.0333):
-            err_trans = compute_err(param_delta,param_delay,traj_gt,traj_est,stamps_gt,stamps_est)    
-            print param_delta, " ".join(str(f) for f in err_trans)
-        sys.exit()
+#    if args.evaldelta:
+#        for param_delta in numpy.arange(0,10,0.0333):
+#            err_trans = compute_err(param_delta,param_delay,traj_gt,traj_est,stamps_gt,stamps_est)    
+#            print param_delta, " ".join(str(f) for f in err_trans)
+#        sys.exit()
+#
+#    if args.evaldelay:
+#        for param_delay in numpy.arange(-0.1,0.1,0.01):
+#            err_trans = compute_err(param_delta,param_delay,traj_gt,traj_est,stamps_gt,stamps_est)    
+#            print param_delay, " ".join(str(f) for f in err_trans)
+#        sys.exit()
 
-    if args.evaldelay:
-        for param_delay in numpy.arange(-0.1,0.1,0.01):
-            err_trans = compute_err(param_delta,param_delay,traj_gt,traj_est,stamps_gt,stamps_est)    
-            print param_delay, " ".join(str(f) for f in err_trans)
-        sys.exit()
-
-    err_trans = compute_err(param_delta,param_delay,traj_gt,traj_est,stamps_gt,stamps_est)    
-    print "translational error =", " ".join(str(f) for f in err_trans)
+    output =[]
+    #output.append("trajectory.groundtruth=%s"%args.groundtruth)
+    #output.append("trajectory.estimated=%s"%args.estimated)
+    output.append("parameter.time_delta=%0.3fs"%param_delta)
+    #output.append("parameter.time_delay=%0.3fs"%param_delay)
+    output = output + compute_err(param_delta,param_delay,traj_gt,traj_est,stamps_gt,stamps_est)    
+    print "\n".join(str(f) for f in output)
