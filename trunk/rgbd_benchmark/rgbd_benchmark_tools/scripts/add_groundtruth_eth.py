8f3bdbcddf90e8db8447eee09bac33f9178c2a4b
sturmju <sturmju@1019801f-97d9-4986-93c4-4f567f763985>
2011-08-09 05:04:31 +0000

    renamed load manifest header

--- trunk/rgbd_benchmark/rgbd_benchmark_tools/scripts/add_groundtruth_eth.py
+++ trunk/rgbd_benchmark/rgbd_benchmark_tools/scripts/add_groundtruth_eth.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-import roslib; roslib.load_manifest('kinect_bagtools')
+import roslib; roslib.load_manifest('rgbd_benchmark_tools')
 import rosbag
 import tf, tf.msg
 import geometry_msgs.msg
