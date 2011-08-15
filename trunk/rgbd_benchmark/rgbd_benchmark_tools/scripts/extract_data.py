51a53c2853c14fffb40d779a4bfb23858a815d63
sturmju <sturmju@1019801f-97d9-4986-93c4-4f567f763985>
2011-08-15 07:01:53 +0000

    update

--- trunk/rgbd_benchmark/rgbd_benchmark_tools/scripts/extract_data.py
+++ trunk/rgbd_benchmark/rgbd_benchmark_tools/scripts/extract_data.py
@@ -27,15 +27,17 @@ if __name__ == '__main__':
     args = parser.parse_args()
     
     rgbfile = os.path.splitext(args.inputbag)[0] + "-rgb"
+    rgbstamps = os.path.splitext(args.inputbag)[0] + "-rgb.txt"
     if not os.path.isdir(rgbfile): 
         os.mkdir(rgbfile)
-    depthfile16 = os.path.splitext(args.inputbag)[0] + "-depth16"
+    depthfile16 = os.path.splitext(args.inputbag)[0] + "-depth"
+    depthstamps = os.path.splitext(args.inputbag)[0] + "-depth.txt"
     if not os.path.isdir(depthfile16): 
         os.mkdir(depthfile16)
     depthfile8 = os.path.splitext(args.inputbag)[0] + "-depth8"
     if not os.path.isdir(depthfile8): 
         os.mkdir(depthfile8)
-    imufile = os.path.splitext(args.inputbag)[0] + "-imu.txt"
+    imufile = os.path.splitext(args.inputbag)[0] + "-accelerometer.txt"
       
     print "Processing bag file:"
     print "  in:",args.inputbag
@@ -56,10 +58,20 @@ if __name__ == '__main__':
     time_start = None
     depth_image=0
     imufile = open(imufile,"w")
+    rgbstamps = open(rgbstamps,"w")
+    depthstamps = open(depthstamps,"w")
     imufile.write("""# accelerometer data
 # file: '%s'
-# time,ax,ay,az
-"""%args.inputbag)
+# timestamp,ax,ay,az
+"""%os.path.basename(args.inputbag))
+    rgbstamps.write("""# color images
+# file: '%s'
+# timestamp,filename
+"""%os.path.basename(args.inputbag))
+    depthstamps.write("""# depth maps
+# file: '%s'
+# timestamp,filename
+"""%os.path.basename(args.inputbag))
     for topic, msg, t in inbag.read_messages():
         if time_start==None:
             time_start=t
@@ -89,16 +101,24 @@ if __name__ == '__main__':
                     except:
                         img[v,u] = 0
             cv.SaveImage(depthfile16+"/%f.png"%depth_image.header.stamp.to_sec(),img)
+            depthstamps.write("%+5.4f, %s\n"%(
+		depth_image.header.stamp.to_sec(),
+		os.path.basename(depthfile16)+"/%f.png"%depth_image.header.stamp.to_sec()))
         if topic == "/camera/rgb/image_color":
             rgb_image_color = msg
             cv_rgb_image_color = bridge.imgmsg_to_cv(rgb_image_color, "bgr8")
-            cv.SaveImage(rgbfile+"/%f.png"%rgb_image_color.header.stamp.to_sec(),cv_rgb_image_color)
+            cv.SaveImage(os.path.basename(rgbfile)+"/%f.png"%rgb_image_color.header.stamp.to_sec(),cv_rgb_image_color)
+            rgbstamps.write("%+5.4f, %s\n"%(
+		rgb_image_color.header.stamp.to_sec(),
+		rgbfile+"/%f.png"%rgb_image_color.header.stamp.to_sec()))
         if topic == "/imu":
-            imufile.write("%+5.4f,    %+1.4f, %+1.4f, %+1.4f\n"%
+            imufile.write("%+5.4f, %+1.4f, %+1.4f, %+1.4f\n"%
                           (msg.header.stamp.to_sec(),
                            msg.linear_acceleration.x,
                            msg.linear_acceleration.y,
                            msg.linear_acceleration.z))
     print
     imufile.close()
-    
+    rgbstamps.close()
+    depthstamps.close()
+
