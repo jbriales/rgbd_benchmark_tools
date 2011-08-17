afc5c4a5cef96a3b47cdb7cad37b454849c359b9
sturmju <sturmju@1019801f-97d9-4986-93c4-4f567f763985>
2011-08-17 18:14:18 +0000

    website, ir

--- trunk/rgbd_benchmark/rgbd_benchmark_tools/scripts/make_website.py
+++ trunk/rgbd_benchmark/rgbd_benchmark_tools/scripts/make_website.py
@@ -9,7 +9,7 @@ datasets = [
                  "freiburg1/floor",
                  "freiburg2/xyz",
                  "freiburg2/rpy"]],
-            ["SLAM (Simulatenous Localization and Mapping) Datasets",
+            ["SLAM Evaluation Datasets",
                 ["freiburg1/desk",
                  "freiburg1/desk2",
                  "freiburg1/room",
@@ -33,7 +33,7 @@ datasets = [
                  "freiburg2/kugel",
                  "freiburg2/kugel2",
                  ]],
-            ["Calibration/Other Datasets",
+            ["Calibration Datasets",
                 ["freiburg1/rgb_calibration",
                  "freiburg1/ir_calibration",
                  "freiburg2/rgb_calibration",
@@ -44,19 +44,26 @@ datasets = [
 
 files=[]
 html = ""
-summary = "<table>\n"
+summary = '''<div class="toc"> 
+<div class="tocheader toctoggle" id="toc__header">Table of Contents</div> 
+<div id="toc__inside"> 
+<ul class="toc">\n'''
 for category,sets in datasets:
     print "category '%s'"%category
-    summary+="  <tr><td valign='top'><a href='%s'>%s</a></td><td>\n"%(category,category)
+    summary+='<li class="level1"><div class="li"><span class="li"><a href="#%s" class="toc">%s</a></span></div>'%(category,category)
+    summary+='<ul class="toc"> '
     html += "\n<h2><a name='%s'>%s</a></h2>\n"%(category,category)
     for s in sets:
         print "  dataset '%s'"%s
         d = s.split("/")
         bagfile = "/usr/wiss/sturmju/public_html/dirs/rgbd_datasets/" + d[0]+ "/rgbd_dataset_"+d[0]+"_"+d[1]+".bag"
         html += generate_wikitext.generate_wikitext(bagfile,s)
-        summary += "    <a href='%s'>%s</a><br>\n"%(s,s)
-    summary+="  </td></tr>\n"
-summary+="</table>\n"
+        summary+='<li class="level2"><div class="li"><span class="li"><a href="#%s" class="toc">%s</a></span></div></li>'%(d[0]+"_"+d[1],d[0]+"_"+d[1])
+    summary+="</li></ul>"
+summary+="</ul></div></div>"
 f = open("/usr/wiss/sturmju/public_html/dirs/rgbd_datasets/index.html","w")
-f.write(summary+html)
+f.write(html)
+f.close()
+f = open("/usr/wiss/sturmju/public_html/dirs/rgbd_datasets/toc.html","w")
+f.write(summary)
 f.close()
