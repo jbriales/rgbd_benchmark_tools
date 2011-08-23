d412b82f5a1300c2c45e2892fcf4ad820a53fa05
sturmju <sturmju@1019801f-97d9-4986-93c4-4f567f763985>
2011-08-23 08:11:24 +0000

    evaluation script

--- trunk/rgbd_benchmark/rgbd_benchmark_tools/scripts/evaluate.py
+++ trunk/rgbd_benchmark/rgbd_benchmark_tools/scripts/evaluate.py
@@ -38,19 +38,19 @@ def read_trajectory(filename):
     traj = dict([(l[0],transform44(l[0:])) for l in list if l[4:8]!=[0,0,0,0]])
     return traj
 
-def find_closest_stamp(stamps,t):
+def find_closest_index(L,t):
     beginning = 0
-    difference = abs(stamps[0] - t)
-    best = stamps[0]
-    end = len(stamps)
+    difference = abs(L[0] - t)
+    best = 0
+    end = len(L)
     while beginning < end:
         middle = int((end+beginning)/2)
-        if abs(stamps[middle] - t) < difference:
-            difference = abs(stamps[middle] - t)
-            best = stamps[middle]
-        if t == stamps[middle]:
-            return stamps[middle]
-        elif stamps[middle] > t:
+        if abs(L[middle] - t) < difference:
+            difference = abs(L[middle] - t)
+            best = middle
+        if t == L[middle]:
+            return middle
+        elif L[middle] > t:
             end = middle
         else:
             beginning = middle + 1
@@ -59,24 +59,68 @@ def find_closest_stamp(stamps,t):
 def ominus(a,b):
     return numpy.dot(numpy.linalg.inv(a),b)
 
-def evaluate_trajectory(traj_gt,traj_est,param_delta=1.00,param_offset=0.01,store_individual_errors=False):
+def compute_distance(transform):
+    return numpy.linalg.norm(transform[0:3,3])
+
+def compute_angle(transform):
+    # an invitation to 3-d vision, p 27
+    return numpy.arccos( min(1,max(-1, (numpy.trace(transform[0:3,0:3]) - 1)/2) ))
+
+def distances_along_trajectory(traj):
+    keys = traj.keys()
+    keys.sort()
+    motion = [ominus(traj[keys[i+1]],traj[keys[i]]) for i in range(len(keys)-1)]
+    distances = [0]
+    sum = 0
+    for t in motion:
+        sum += compute_distance(t)
+        distances.append(sum)
+    return distances
+    
+def rotations_along_trajectory(traj):
+    keys = traj.keys()
+    keys.sort()
+    motion = [ominus(traj[keys[i+1]],traj[keys[i]]) for i in range(len(keys)-1)]
+    distances = [0]
+    sum = 0
+    for t in motion:
+        sum += compute_angle(t)
+        distances.append(sum)
+    return distances
+    
+
+def evaluate_trajectory(traj_gt,traj_est,param_delta=1.00,param_delta_unit="s",param_offset=0.01,store_individual_errors=False):
     stamps_gt = list(traj_gt.keys())
-    stamps_gt.sort()
     stamps_est = list(traj_est.keys())
+    stamps_gt.sort()
     stamps_est.sort()
 
+    if param_delta_unit=="s":
+        index_gt = list(traj_gt.keys())
+        index_est = list(traj_est.keys())
+        index_gt.sort()
+        index_est.sort()
+    elif param_delta_unit=="m":
+        index_gt = distances_along_trajectory(traj_gt)
+        index_est = distances_along_trajectory(traj_est)
+    elif param_delta_unit=="rad":
+        index_gt = rotations_along_trajectory(traj_gt)
+        index_est = rotations_along_trajectory(traj_est)
+    else:
+        raise Exception("Unknown unit for delta: '%s'"%param_delta_unit)
+    
     result ={}
-    result["parameter.time_delta"] = (float(param_delta),"s")
-    result["parameter.time_offset"] = (float(param_offset),"s")
+    result["parameter.delta"] = (float(param_delta),param_delta_unit)
+    result["parameter.offset"] = (float(param_offset),"s")
 
     interval_gt = [abs(a-b) for a,b in zip(stamps_gt[:-1],stamps_gt[1:])]
     interval_est = [abs(a-b) for a,b in zip(stamps_est[:-1],stamps_est[1:])]
     
     result["input.groundtruth.samples"] = (float(len(stamps_gt)),"samples")
     result["input.estimated.samples"] = (float(len(stamps_est)),"samples")
-    result["input.groundtruth.interval.median"] = (numpy.median(interval_gt),"s")
+    result["input.groundtruth.frequency"] = (1/numpy.median(interval_gt),"Hz")
     result["input.groundtruth.duration"] = (len(stamps_gt) * numpy.median(interval_gt),"s")
-    result["input.estimated.interval.median"] = (numpy.median(interval_est),"s")
+    result["input.estimated.frequency"] = (1/numpy.median(interval_est),"Hz")
     result["input.estimated.duration"] = (len(stamps_est) * numpy.median(interval_est),"s")
     result["input.coverage"] = (result["input.estimated.duration"][0] / result["input.groundtruth.duration"][0],"")
     
@@ -84,25 +128,27 @@ def evaluate_trajectory(traj_gt,traj_est,param_delta=1.00,param_offset=0.01,stor
     err_rot = []
     
     matches_difference = []
-    for stamp_est_0 in stamps_est:
-        if stamp_est_0+param_delta > stamps_est[-1]: 
+    for i in range(len(traj_est)):
+        j = find_closest_index(index_est,index_est[i] + param_delta)
+        if j==len(traj_est)-1: 
             continue
-        stamp_est_1 = find_closest_stamp(stamps_est,stamp_est_0 + param_delta)
-        stamp_gt_0 = find_closest_stamp(stamps_gt,stamp_est_0 - param_offset)
-        stamp_gt_1 = find_closest_stamp(stamps_gt,stamp_est_1 - param_offset)
+        
+        stamp_est_0 = stamps_est[i]
+        stamp_est_1 = stamps_est[j]
+
+        stamp_gt_0 = stamps_gt[ find_closest_index(stamps_gt,stamp_est_0 - param_offset) ]
+        stamp_gt_1 = stamps_gt[ find_closest_index(stamps_gt,stamp_est_1 - param_offset) ]
         
         matches_difference.append( abs(stamp_est_0 - stamp_gt_0) )
         matches_difference.append( abs(stamp_est_1 - stamp_gt_1) )
         
-        #print stamp_est_0,stamp_est_1,stamp_gt_0,stamp_gt_1
-        
         error44 = ominus(  ominus( traj_est[stamp_est_1], traj_est[stamp_est_0] ),
                            ominus( traj_gt[stamp_gt_1], traj_gt[stamp_gt_0] ) )
         
-        trans = numpy.linalg.norm(error44[0:3,3])
+        trans = compute_distance(error44)
         err_trans.append( trans )
-        # an invitation to 3-d vision, p 27
-        rot = numpy.arccos( min(1,max(-1, (numpy.trace(error44[0:3,0:3]) - 1)/2) ))
+
+        rot = compute_angle(error44)
         err_rot.append( rot )
         
         if store_individual_errors:
@@ -144,19 +190,21 @@ if __name__ == '__main__':
     ''')
     parser.add_argument('groundtruth_file', help='ground-truth trajectory file (format: "timestamp tx ty tz qx qy qz qw")')
     parser.add_argument('estimated_file', help='estimated trajectory file (format: "timestamp tx ty tz qx qy qz qw")')
-    parser.add_argument('--delta', help='time delta for evaluation (default: 1.0)',default=1.0)
+    parser.add_argument('--delta', help='delta for evaluation (default: 1.0)',default=1.0)
+    parser.add_argument('--delta_unit', help='unit of delta (options: \'s\' for seconds, \'m\' for meters, \'rad\' for radians; default: \'s\')',default='s')
     parser.add_argument('--offset', help='time offset between ground-truth and estimated trajectory (default: 0.0)',default=0.0)
     parser.add_argument('--verbose', help='print all evaluation data (otherwise, only the mean translational error measured in meters will be printed)', action='store_true')
     parser.add_argument('--store_individual_errors', help='additional store the individual errors (use with --verbose)', action='store_true')
     args = parser.parse_args()
     
     param_delta = float(args.delta)
+    param_delta_unit = args.delta_unit
     param_offset = float(args.offset)
     
     traj_gt = read_trajectory(args.groundtruth_file)
     traj_est = read_trajectory(args.estimated_file)
     
-    result = evaluate_trajectory(traj_gt,traj_est,param_delta,param_offset,args.store_individual_errors)
+    result = evaluate_trajectory(traj_gt,traj_est,param_delta,param_delta_unit,param_offset,args.store_individual_errors)
     if args.verbose:
         keys = list(result.keys())
         keys.sort()
