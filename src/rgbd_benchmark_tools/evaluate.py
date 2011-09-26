819603456a1eacffa9d7fe9e2d29bf801e97350b
sturmju <sturmju@1019801f-97d9-4986-93c4-4f567f763985>
2011-09-26 09:57:01 +0000

    unified plot scripts

--- src/rgbd_benchmark_tools/evaluate.py
+++ src/rgbd_benchmark_tools/evaluate.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python
-#
-# Requirements: 
-# sudo apt-get install python-argparse
 
 import argparse
-import sys
-import os
-import numpy
 import random
+import numpy
+import sys
 
 _EPS = numpy.finfo(float).eps * 4.0
 
@@ -193,7 +189,7 @@ if __name__ == '__main__':
     random.seed(0)
 
     parser = argparse.ArgumentParser(description='''
-    This script reads a ground-truth trajectory and an estimated trajectory, and computes -- by default -- the root mean squared translational error.
+    This script reads a ground-truth trajectory and an estimated trajectory, and computes the (translational) error.
     ''')
     parser.add_argument('groundtruth_file', help='ground-truth trajectory file (format: "timestamp tx ty tz qx qy qz qw")')
     parser.add_argument('estimated_file', help='estimated trajectory file (format: "timestamp tx ty tz qx qy qz qw")')
@@ -201,12 +197,15 @@ if __name__ == '__main__':
     parser.add_argument('--fixed_delta', help='only consider pose pairs that have a distance of delta delta_unit (e.g., for evaluating the drift per second/meter/radian)', action='store_true')
     parser.add_argument('--delta', help='delta for evaluation (default: 1.0)',default=1.0)
     parser.add_argument('--delta_unit', help='unit of delta (options: \'s\' for seconds, \'m\' for meters, \'rad\' for radians, \'f\' for frames; default: \'s\')',default='s')
-    parser.add_argument('--offset', help='time offset added to the timestamps of the estimated trajectory (default: 0.0)',default=0.0)
+    parser.add_argument('--offset', help='time offset between ground-truth and estimated trajectory (default: 0.0)',default=0.0)
     parser.add_argument('--scale', help='scaling factor for the estimated trajectory (default: 1.0)',default=1.0)
     parser.add_argument('--save', help='text file to which the evaluation will be saved (format: stamp_est0 stamp_est1 stamp_gt0 stamp_gt1 trans_error rot_error)')
-    parser.add_argument('--percentiles', help='number of percentiles to evaluate (default: disabled)', default=0)
-    parser.add_argument('--verbose', help='print all evaluation data (otherwise, only the RMSE translational error measured in meters will be printed)', action='store_true')
+    parser.add_argument('--plot', help='plot the result to a file (requires --fixed_delta, output format: png)')
+    parser.add_argument('--verbose', help='print all evaluation data (otherwise, only the mean translational error measured in meters will be printed)', action='store_true')
     args = parser.parse_args()
+    
+    if args.plot and not args.fixed_delta:
+        sys.exit("The '--plot' option can only be used in combination with '--fixed_delta'")
     
     traj_gt = read_trajectory(args.groundtruth_file)
     traj_est = read_trajectory(args.estimated_file)
@@ -220,6 +219,7 @@ if __name__ == '__main__':
                                  float(args.offset),
                                  float(args.scale))
     
+    stamps = numpy.array(result)[:,0]
     trans_error = numpy.array(result)[:,4]
     rot_error = numpy.array(result)[:,5]
     
@@ -237,12 +237,6 @@ if __name__ == '__main__':
         print "translational_error.std %f m"%numpy.std(trans_error)
         print "translational_error.min %f m"%numpy.min(trans_error)
         print "translational_error.max %f m"%numpy.max(trans_error)
-        
-        percentiles = int(args.percentiles)
-        if(percentiles>0):
-            for p in range(percentiles+1):
-                q = 1.0 / (percentiles) * p
-                print "translational_error.percentile.%0.2f %f m"%(q,percentile(trans_error,q))
 
         print "rotational_error.rmse %f deg"%(numpy.sqrt(numpy.dot(rot_error,rot_error) / len(rot_error)) * 180.0 / numpy.pi)
         print "rotational_error.mean %f deg"%(numpy.mean(rot_error) * 180.0 / numpy.pi)
@@ -250,12 +244,20 @@ if __name__ == '__main__':
         print "rotational_error.std %f deg"%(numpy.std(rot_error) * 180.0 / numpy.pi)
         print "rotational_error.min %f deg"%(numpy.min(rot_error) * 180.0 / numpy.pi)
         print "rotational_error.max %f deg"%(numpy.max(rot_error) * 180.0 / numpy.pi)
-
-        if(percentiles>0):
-            percentiles = int(args.percentiles)
-            for p in range(percentiles+1):
-                q = 1.0 / (percentiles) * p
-                print "translational_error.percentile.%0.2f %f m"%(q,percentile(rot_error,q))
-
     else:
-        print numpy.sqrt(numpy.dot(trans_error,trans_error) / len(trans_error))
+        print numpy.mean(trans_error)
+
+    if args.plot:    
+        import matplotlib
+        matplotlib.use('Agg')
+        import matplotlib.pyplot as plt
+        import matplotlib.pylab as pylab
+        fig = plt.figure()
+        ax = fig.add_subplot(111)        
+        ax.plot(stamps - stamps[0],trans_error,'-',color="blue")
+        #ax.plot([t for t,e in err_rot],[e for t,e in err_rot],'-',color="red")
+        ax.set_xlabel('time [s]')
+        ax.set_ylabel('translational error [m]')
+        plt.savefig(args.plot,dpi=90)
+        
+
