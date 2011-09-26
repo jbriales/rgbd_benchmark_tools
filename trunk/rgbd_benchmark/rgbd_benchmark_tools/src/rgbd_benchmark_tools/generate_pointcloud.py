aba4a54dd95e14ac7bb367087a8367181e18bebe
sturmju <sturmju@1019801f-97d9-4986-93c4-4f567f763985>
2011-09-26 11:38:26 +0000

    update

--- trunk/rgbd_benchmark/rgbd_benchmark_tools/src/rgbd_benchmark_tools/generate_pointcloud.py
+++ trunk/rgbd_benchmark/rgbd_benchmark_tools/src/rgbd_benchmark_tools/generate_pointcloud.py
@@ -13,18 +13,9 @@ centerX = 319.5
 centerY = 239.5
 scalingFactor = 5000.0
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='''
-    This script reads a registered pair of color and depth images and generates a colored 3D point cloud in the
-    PLY format. 
-    ''')
-    parser.add_argument('rgb_file', help='input color image (format: png)')
-    parser.add_argument('depth_file', help='input depth image (format: png)')
-    parser.add_argument('ply_file', help='output PLY file (format: ply)')
-    args = parser.parse_args()
-
-    rgb = Image.open(args.rgb_file)
-    depth = Image.open(args.depth_file)
+def generate_pointcloud(rgb_file,depth_file,ply_file):
+    rgb = Image.open(rgb_file)
+    depth = Image.open(depth_file)
     
     if rgb.size != depth.size:
         raise Exception("Color and depth image do not have the same resolution.")
@@ -38,13 +29,12 @@ if __name__ == '__main__':
     for v in range(rgb.size[1]):
         for u in range(rgb.size[0]):
             color = rgb.getpixel((u,v))
-            color32 = color[0] + (color[1]<<8) + (color[2]<<16)
             Z = depth.getpixel((u,v)) / scalingFactor
             if Z==0: continue
             X = (u - centerX) * Z / focalLength
             Y = (v - centerY) * Z / focalLength
             points.append("%f %f %f %d %d %d 0\n"%(X,Y,Z,color[0],color[1],color[2]))
-    file = open(args.ply_file,"w")
+    file = open(ply_file,"w")
     file.write('''ply
 format ascii 1.0
 element vertex %d
@@ -59,3 +49,16 @@ end_header
 %s
 '''%(len(points),"".join(points)))
     file.close()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='''
+    This script reads a registered pair of color and depth images and generates a colored 3D point cloud in the
+    PLY format. 
+    ''')
+    parser.add_argument('rgb_file', help='input color image (format: png)')
+    parser.add_argument('depth_file', help='input depth image (format: png)')
+    parser.add_argument('ply_file', help='output PLY file (format: ply)')
+    args = parser.parse_args()
+
+    generate_pointcloud(args.rgb_file,args.depth_file,args.ply_file)
+    
