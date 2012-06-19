c2e67035f0bfe14561422f4473e8cdaab9e20a74
endres <endres@1019801f-97d9-4986-93c4-4f567f763985>
2012-06-19 16:23:25 +0000

    support for PCD Format

--- src/rgbd_benchmark_tools/generate_registered_pointcloud.py
+++ src/rgbd_benchmark_tools/generate_registered_pointcloud.py
@@ -10,11 +10,33 @@ from associate import *
 from evaluate_rpe import *
 from generate_pointcloud import *
 from PIL import Image
+import struct
 
 focalLength = 525.0
 centerX = 319.5
 centerY = 239.5
 scalingFactor = 5000.0
+
+def write_pcd(pcd_file,points,pose):
+    file = open(pcd_file,"wb")
+    file.write('''# .PCD v.7 - Point Cloud Data file format
+VERSION .7
+FIELDS x y z rgb
+SIZE 4 4 4 4
+TYPE F F F F
+COUNT 1 1 1 1
+WIDTH %d
+HEIGHT 1
+VIEWPOINT %f %f %f %f %f %f %f 
+POINTS %d
+DATA binary
+%s
+'''%(len(points), pose[0], pose[1], pose[2], pose[6], pose[3], pose[4], pose[5]                         
+,len(points),"".join(points)))
+    file.close()
+    print "Saved %d points to '%s'"%(len(points),pcd_file)
+
+
 
 def write_ply(ply_file,points):
     file = open(ply_file,"w")
@@ -34,7 +56,8 @@ end_header
     file.close()
     print "Saved %d points to '%s'"%(len(points),ply_file)
 
-def generate_pointcloud(rgb_file,depth_file,transform,downsample):
+
+def generate_pointcloud(rgb_file,depth_file,transform,downsample,pcd=False):
     rgb = Image.open(rgb_file)
     depth = Image.open(depth_file)
     
@@ -55,8 +78,11 @@ def generate_pointcloud(rgb_file,depth_file,transform,downsample):
             X = (u - centerX) * Z / focalLength
             Y = (v - centerY) * Z / focalLength
             vec_org = numpy.matrix([[X],[Y],[Z],[1]])
-            vec_transf = numpy.dot(transform,vec_org)
-            points.append("%f %f %f %d %d %d 0\n"%(vec_transf[0,0],vec_transf[1,0],vec_transf[2,0],color[0],color[1],color[2]))
+            if pcd:
+              points.append(struct.pack("fffI",vec_org[0,0],vec_org[1,0],vec_org[2,0],color[0]*2**16+color[1]*2**8+color[2]*2**0))
+            else:
+              vec_transf = numpy.dot(transform,vec_org)
+              points.append("%f %f %f %d %d %d 0\n"%(vec_transf[0,0],vec_transf[1,0],vec_transf[2,0],color[0],color[1],color[2]))
             
     return points
 
@@ -75,8 +101,9 @@ if __name__ == '__main__':
     parser.add_argument('--downsample', help='downsample images by this factor (default: 1)',default=1)
     parser.add_argument('--nth', help='only consider every nth image pair (default: 1)',default=1)
     parser.add_argument('--individual', help='save individual point clouds (instead of one large point cloud)', action='store_true')
+    parser.add_argument('--pcd_format', help='Write pointclouds in pcd format (implies --individual)', action='store_true')
     
-    parser.add_argument('ply_file', help='output PLY file (format: ply)')
+    parser.add_argument('output_file', help='output PLY file (format: ply)')
     args = parser.parse_args()
 
     rgb_list = read_file_list(args.rgb_list)
@@ -87,19 +114,39 @@ if __name__ == '__main__':
     matches_rgb_traj = associate(matches_rgb_depth, pose_list,float(args.traj_offset),float(args.traj_max_difference))
     matches_rgb_traj.sort()
     
-    traj = read_trajectory(args.trajectory_file)
+    if args.pcd_format:
+      args.individual = True
+      traj = read_trajectory(args.trajectory_file, False)
+    else:
+      traj = read_trajectory(args.trajectory_file)
     
     all_points = []
     list  = range(0,len(matches_rgb_traj),int(args.nth))
     for frame,i in enumerate(list):
         rgb_stamp,traj_stamp = matches_rgb_traj[i]
+
+        if args.individual:
+          if args.pcd_format:
+            out_filename = "%s-%f.pcd"%(os.path.splitext(args.output_file)[0],rgb_stamp)
+          else:
+            out_filename = "%s-%f.ply"%(os.path.splitext(args.output_file)[0],rgb_stamp)
+          if os.path.exists(out_filename):
+            print "skipping existing cloud file ", out_filename
+            continue
+
         rgb_file = rgb_list[rgb_stamp][0]
         depth_file = depth_list[matches_rgb_depth[rgb_stamp]][0]
         pose = traj[traj_stamp]
-        points = generate_pointcloud(rgb_file,depth_file,pose,int(args.downsample))
+        points = generate_pointcloud(rgb_file,depth_file,pose,int(args.downsample), args.pcd_format)
+
         if args.individual:
-            write_ply("%s-%f.ply"%(os.path.splitext(args.ply_file)[0],rgb_stamp),points)
+          if args.pcd_format:
+            write_pcd(out_filename,points,pose)
+          else:
+            write_ply(out_filename,points)
         else:
             all_points += points
             print "Frame %d/%d, number of points so far: %d"%(frame+1,len(list),len(all_points))
-    write_ply(args.ply_file,all_points)
+
+    if not args.individual:
+      write_ply(args.output_file,all_points)
