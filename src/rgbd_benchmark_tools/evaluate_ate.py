8c16523becb7a5bcb799925d0883dcd62174a250
nobody <nobody@1019801f-97d9-4986-93c4-4f567f763985>
2012-09-12 16:40:28 +0000

    fixed

--- src/rgbd_benchmark_tools/evaluate_ate.py
+++ src/rgbd_benchmark_tools/evaluate_ate.py
@@ -178,10 +178,10 @@ if __name__=="__main__":
         plot_traj(ax,first_stamps,first_xyz_full.transpose().A,'-',"black","ground truth")
         plot_traj(ax,second_stamps,second_xyz_full_aligned.transpose().A,'-',"blue","estimated")
 
-        #label="difference"
-        #for (a,b),(x1,y1,z1),(x2,y2,z2) in zip(matches,first_xyz.transpose().A,second_xyz_aligned.transpose().A):
-        #    ax.plot([x1,x2],[y1,y2],'-',color="red",label=label)
-        #    label=""
+        label="difference"
+        for (a,b),(x1,y1,z1),(x2,y2,z2) in zip(matches,first_xyz.transpose().A,second_xyz_aligned.transpose().A):
+            ax.plot([x1,x2],[y1,y2],'-',color="red",label=label)
+            label=""
             
         ax.legend()
             
